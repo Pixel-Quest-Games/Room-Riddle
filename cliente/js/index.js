dcdbb7bf3d797559e26d787b27d66ffe06154b0e
Cleiton Bernardo Batista <cleiton.b2004@aluno.ifsc.edu.br>
2023-08-23 20:19:18 +0000

    Instalação ESLint

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -4,15 +4,15 @@ import cena0 from './cena0.js'
 
 /* Definição do objeto Game */
 class Game extends Phaser.Game {
-    constructor () {
-        super(config)
+  constructor () {
+    super(config)
 
-        this.scene.add('cena0', cena0)
-        this.scene.start('cena0')
-    }
+    this.scene.add('cena0', cena0)
+    this.scene.start('cena0')
+  }
 }
 
 /* Criação do objeto Game */
 window.onload = () => {
-    window.game = new Game()
+  window.game = new Game()
 }
