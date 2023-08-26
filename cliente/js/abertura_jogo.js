b3d6f4c338d59ccaf23f7f8c6502987046a66d88
Pedro Augusto Pereira Rodrigues <ellisangelaaugusto2005@gmail.com>
2023-08-26 18:56:06 +0000

    Adicionar botao

--- cliente/js/abertura_jogo.js
+++ cliente/js/abertura_jogo.js
@@ -6,13 +6,13 @@ export default class abertura_jogo extends Phaser.Scene {
 
   preload() {
     this.load.image('background1', '../assets/imagens/background1.png')
-    this.load.image('botao_jogar', '../assets/imagens/botao_jogar.png')
+    this.load.image('botao_start', '../assets/imagens/botao_start.png')
   }
 
   /* add image(400,225, ...) por ser pela metade, começa pelo meio */
   create() {
     this.add.image(400, 225, 'background1')
-    this.jogar = this.add.image(400, 300, 'botao_jogar')
+    this.jogar = this.add.image(400, 250, 'botao_start')
       .setInteractive()
       .on('pointerdown', () => {
         this.game.scene.stop('abertura_jogo')
