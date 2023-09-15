b7c70a4bb0c449e04c3750db8d896deec505d75d
Pedro Augusto Pereira Rodrigues <ellisangelaaugusto2005@gmail.com>
2023-09-15 20:06:51 +0000

    atualização da lógica do enigma final

--- cliente/js/enigma_final.js
+++ cliente/js/enigma_final.js
@@ -8,6 +8,9 @@ export default class enigma_final extends Phaser.Scene {
     this.load.image('enigma_final_tela_cheia', '../assets/imagens/enigma_final_tela_cheia.png')
     this.load.image('seta_down', '../assets/imagens/seta_down.png')
     this.load.image('buttom_down', '../assets/imagens/seta_down_enigma_final.png')
+    this.load.image('vverde', '../assets/imagens/v_verde.png')
+    this.load.image('iverde', '../assets/imagens/i_verde.png')
+    this.load.image('averde', '../assets/imagens/a_verde.png')
     this.load.spritesheet('alfabeto_spritesheet', '../assets/imagens/alfabeto_spritesheet.png', {
       frameWidth: 176,
       frameHeight: 176
@@ -75,6 +78,16 @@ export default class enigma_final extends Phaser.Scene {
         y: 333
       }
     ]
+/*
+    const verifica = (x, y) => {
+      if (this.botoes[index].numero === x && this.alfabeto[index].objeto.frame.name === y) {
+        
+      }
+    }*/
+
+
+
+    
     this.verificacao4 = 'V'
     this.botoes.forEach((item, index) => {
       item.botao = this.add.image(item.x, item.y, 'buttom_down')
@@ -82,44 +95,56 @@ export default class enigma_final extends Phaser.Scene {
         .on('pointerdown', () => {
           this.alfabeto[index].objeto.setFrame(this.alfabeto[index].objeto.frame.name + 1)
           console.log(this.botoes[index].numero)
+          console.log(this.alfabeto[index].objeto.frame.name)
+
           //Verifica primeira caixa
-          if (this.botoes[index].numero = '1' && this.alfabeto[index].objeto.frame.name == 21) {
-            console.log('deu certo')
+          if (this.botoes[index].numero == '1') {
+            if (this.alfabeto[index].objeto.frame.name == 21) {
+              console.log('deu certo')
             this.verificacao1 = 'V' 
           } else {
             console.log('falsooo')
             this.verificacao1 = 'F'
           }
+            }
           //Verifica segunda caixa
-          if (this.botoes[index].numero = '2' && this.alfabeto[index].objeto.frame.name == 8) {
-            console.log('deu certo')
-            this.verificacao2 = 'V'
-          } else {
-            console.log('falsooo')
-            this.verificacao2 = 'F'
+          if (this.botoes[index].numero == '2') {
+            if (this.alfabeto[index].objeto.frame.name == 8) {
+              console.log('deu certo')
+              this.verificacao2 = 'V'
+            } else {
+              console.log('falsooo')
+              this.verificacao2 = 'F'
+            }
           }
           //Verifica terceira caixa
-          if (this.botoes[index].numero = '3' && this.alfabeto[index].objeto.frame.name == 21) {
-            console.log('deu certo')
-            this.verificacao3 = 'V'
-          } else {
-            console.log('falsooo')
-            this.verificacao3 = 'F'
+          if (this.botoes[index].numero == '3') {
+            if (this.alfabeto[index].objeto.frame.name == 21) {
+              console.log('deu certo')
+              this.verificacao3 = 'V'
+            } else {
+              console.log('falsooo')
+              this.verificacao3 = 'F'
+            }
           }
           //Verifica quarta caixa
-          if (this.botoes[index].numero = '4' && this.alfabeto[index].objeto.frame.name == 0) {
-            console.log('deu certo')
-            this.verificacao4 = 'V'
-          } else {
-            console.log('falsooo')
-            this.verificacao4 = 'F'
+          if (this.botoes[index].numero == '4') {
+            if (this.alfabeto[index].objeto.frame.name == 0 || this.alfabeto[index].objeto.frame.name === null) {
+              console.log('deu certo')
+              this.verificacao4 = 'V'
+            } else {
+              console.log('falsooo')
+              this.verificacao4 = 'F'
+            }
           }
           //Fazer outro if pra ver se fez o enigma anterior
           if (this.verificacao1 == 'V' && this.verificacao2 == 'V' && this.verificacao3 == 'V' && this.verificacao4 == 'V') {
             console.log('foi meu')
-            this.add.image(200, 200, 'seta_down')
+            this.add.image(173, 230, 'vverde')
+            this.add.image(325, 230, 'iverde')
+            this.add.image(476, 230, 'vverde')
+            this.add.image(622, 230, 'averde')
           }
-          //if(this.botoes[index] & this.botoes.alfabeto)
         })
     })
 
