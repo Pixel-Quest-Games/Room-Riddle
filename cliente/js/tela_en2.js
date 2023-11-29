b98d1886cfefb7451e4603baf3aebbfde2b79c39
Pedro Augusto Pereira Rodrigues <ellisangelaaugusto2005@gmail.com>
2023-11-29 20:00:03 +0000

    Inventario e enigma 2 atualizado

--- cliente/js/tela_en2.js
+++ cliente/js/tela_en2.js
@@ -14,6 +14,8 @@ export default class enigma2 extends Phaser.Scene {
     this.load.image('mulher_inv', '../assets/imagens/boneco_mulher_inv.png')
     this.load.image('entidade', '../assets/imagens/entidade52.png')
     this.load.image('seleciona', '../assets/imagens/seleciona.png')
+    this.load.image('touch', '../assets/imagens/invisivel_p_touch.png')
+    this.load.image('entidade_maior', '../assets/imagens/boneco_entidade0.png')
   }
 
   // Ordem: mas, ent, fem
@@ -23,6 +25,7 @@ export default class enigma2 extends Phaser.Scene {
     this.seleciona1 = this.add.image(121, 360, 'seleciona').setVisible(false)
     this.seleciona2 = this.add.image(183, 360, 'seleciona').setVisible(false)
     this.seleciona3 = this.add.image(245, 360, 'seleciona').setVisible(false)
+    this.mulher_inv = this.add.image(245, 362, 'mulher_inv').setVisible(false)
     if (this.game.inventario1 === true) {
       this.add.image(121, 360, 'entidade')
         .setInteractive()
@@ -33,7 +36,7 @@ export default class enigma2 extends Phaser.Scene {
         })
     }
     if (this.game.inventario3 === true) {
-      this.add.image(245, 362, 'mulher_inv')
+      this.mulher_inv.setVisible(true)
         .setInteractive()
         .on('pointerdown', () => {
           this.seleciona3.setVisible(true)
@@ -41,13 +44,13 @@ export default class enigma2 extends Phaser.Scene {
           this.seleciona1.setVisible(false)
         })
     } else {
-      this.mulher = this.add.image(292, 110, 'mulher')
+      this.mulher_esp1 = this.add.image(292, 110, 'mulher')
         .setInteractive()
         .on('pointerdown', () => {
-          this.mulher.setVisible(false)
+          this.mulher_esp1.setVisible(false)
           this.game.inventario3 = true
           if (this.game.inventario3 === true) {
-            this.add.image(245, 362, 'mulher_inv')
+            this.mulher_inv.setVisible(true)
               .setInteractive()
               .on('pointerdown', () => {
                 this.seleciona3.setVisible(true)
@@ -87,16 +90,44 @@ export default class enigma2 extends Phaser.Scene {
       .on('pointerdown', () => {
         this.game.scene.stop('enigma2')
         this.game.scene.start('sala_m1')
-        if (this.seleciona1.setVisible === true) {
+        if (this.seleciona1.visible === true) {
           this.seleciana1.setVisible(false)
         }
-        if (this.seleciona2.setVisible === true) {
+        if (this.seleciona2.visible === true) {
           this.seleciana2.setVisible(false)
         }
-        if (this.seleciona3.setVisible === true) {
+        if (this.seleciona3.visible === true) {
           this.seleciana3.setVisible(false)
         }
       })
+    this.touch1 = this.add.image(295, 185, 'touch')
+      .setInteractive()
+      .on('pointerdown', () => {
+        console.log('bvfbv')
+        if (this.seleciona1.visible === true) {
+          console.log('sele1')
+          this.add.image(555, 110, 'entidade_maior')
+        }
+        if (this.seleciona2.visible === true) {
+          console.log('sele1')
+          this.homem.setVisible(true)
+        }
+        if (this.seleciona3.visible === true) {
+          this.mulher_esp1.setVisible(true)
+          this.mulher_inv.setVisible(false)
+          this.seleciona3.setVisible(false)
+        }
+      })
+    /* this.touch2 = this.add.image(427, 185, 'touch')
+      .setInteractive()
+      .on('pointerdown', () => {
+
+      })
+    this.touch3 = this.add.image(555, 185, 'touch')
+      .setInteractive()
+      .on('pointerdown', () => {
+
+      }) */
   }
 
   update () {
